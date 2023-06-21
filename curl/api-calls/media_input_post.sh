34d7f1532ad38a26b3921f488a44f29085df8506
Jason Heron <jasonheron@mail.com>
2023-06-21 19:14:21 +0100

    Delete curl directory

--- curl/api-calls/media_input_post.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X POST "https://api.dolby.com/media/input" \
-    --header "x-api-key: $DOLBYIO_API_KEY" \
-    --data '{
-        "url": "dlb://in/example.mp3"
-    }'
--- curl/api-calls/media_input_put.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Pass the pre-signed url and local file on the commandline
-
-curl -X PUT $1 -T $2
